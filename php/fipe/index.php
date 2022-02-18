d73f49260b3f2b240c343af23eaba4575a846c50
Ricardo Pagano <99998824+ricapag2409@users.noreply.github.com>
2022-02-18 18:56:51 -0300

    Add files via upload

---
+++ php/fipe/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fipe WebServices</title>
+  <script src="assets/js/script.js"></script>
+  <link rel="stylesheet" href="assets/css/css.css">
+  <style type="text/css">
+    .tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
+    .tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
+      font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#fff;
+      font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-hmp3{background-color:#D2E4FC;text-align:left;vertical-align:top}
+    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+  </style>
+</head>
+<body>
+<div id="filtros">
+  <label for="anoreferencia">Ano de referência:</label>
+  <select name="" id="anoreferencia"></select>
+  <label for="marca">Marca:</label>
+  <select id="marca"></select>
+  <label for="modelo">Modelo:</label>
+  <select id="modelo"></select>
+  <label for="ano">Ano: </label>
+  <select id="ano"></select>
+</div>
+<div id="resultadoConsulta" style="display:none;margin-top: 20px;"></div>
+
+</body>
+</html>
